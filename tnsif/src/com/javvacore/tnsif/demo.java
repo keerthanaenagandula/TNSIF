303b107c7d57359694d1bf9a1367fb9b0052d494
keerthana <keerthana@localhost>
2025-10-06 22:23:31 +0530

    DEMO

---
+++ tnsif/src/com/javvacore/tnsif/demo.java
@@ -0,0 +1,9 @@
+package com.javvacore.tnsif;
+
+public class demo {
+
+	public static void main(String[] args) {
+		System.out.println("This is tnsif training");
+	}
+
+}
--- tnsif/src/module-info.java
+++ tnsif/src/module-info.java
@@ -4,5 +4,5 @@
 /**
  * 
  */
-module javacoretns {
+module tnsif {
 }

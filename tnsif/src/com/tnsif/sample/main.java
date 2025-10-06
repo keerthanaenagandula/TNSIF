d1d9452e4cc344ae1efd3c2f7582f6111cf9050b
keerthana <keerthana@localhost>
2025-10-07 05:16:03 +0530

    sample

---
+++ tnsif/src/com/tnsif/sample/main.java
@@ -0,0 +1,13 @@
+package com.tnsif.sample;
+
+public class main {
+
+	public static void main(String[] args) {
+		sample ob = new sample ();
+		System.out.println(ob.a);
+		System.out.println(ob.b);
+		ob.show();
+
+	}
+
+}
---
+++ tnsif/src/com/tnsif/sample/sample.java
@@ -0,0 +1,10 @@
+package com.tnsif.sample;
+
+public class sample {
+	int a =20;
+	boolean b = true;
+	public void show ()
+	{
+	System.out.println("This is show method");
+	}
+}

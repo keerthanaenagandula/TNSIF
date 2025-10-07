ce48563033ea387362bcdae11495dbfe458a5ac5
keerthana <keerthana@localhost>
2025-10-07 05:35:55 +0530

    encapsulation

---
+++ tnsif/src/com/tnsif/encapsulation/car.java
@@ -0,0 +1,20 @@
+package com.tnsif.encapsulation;
+
+public class car {
+	String color;
+    String model;
+    int n ;
+	public void start()
+	{
+	System.out.println("car is starteing:");
+	}
+	public void stop()
+		{
+	 System.out.println("Car has stopped:");
+		}
+	public void drifts()
+	{
+	System.out.println("Car has done drifts:");
+	}
+
+}
---
+++ tnsif/src/com/tnsif/encapsulation/main.java
@@ -0,0 +1,16 @@
+package com.tnsif.encapsulation;
+
+public class main {
+
+	public static void main(String[] args) {
+	car ob= new car ();
+	System. out. println(ob.color);
+	System. out. println(ob.model);
+	System. out. println(ob.n) ;
+	ob.start();
+	ob.stop (); 
+	ob.drifts();
+
+	}
+
+}

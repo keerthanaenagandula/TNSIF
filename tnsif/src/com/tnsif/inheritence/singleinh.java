a7f6386b5ef492e5fd1a01bb28c56b6586be7829
keerthana <keerthana@localhost>
2025-10-07 05:49:02 +0530

    single inheritence

---
+++ tnsif/src/com/tnsif/inheritence/singleinh.java
@@ -0,0 +1,13 @@
+package com.tnsif.inheritence;
+
+public class singleinh {
+
+	public static void main(String[] args) {
+		B ob = new B();
+		ob. display ();
+		System.out.println(ob.a ) ;
+		ob. show ( );
+
+	}
+
+}
---
+++ tnsif/src/com/tnsif/inheritence/singleinheritence.java
@@ -0,0 +1,16 @@
+package com.tnsif.inheritence;
+
+public class singleinheritence {
+	int a =10;
+	public void show()
+	{
+	System.out.println (" This is class A");
+	}
+}
+			class B extends singleinheritence {
+			public void display ()
+			{
+			System.out.println(" This is class B ");
+}
+
+			}

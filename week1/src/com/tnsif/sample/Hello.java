aea83684bcd15e487d813fbb0b2b848ee269cdfb
keerthana <keerthana@localhost>
2025-10-09 04:58:26 +0530

    week-1 programs

---
+++ week1/src/com/tnsif/sample/Hello.java
@@ -0,0 +1,13 @@
+package com.tnsif.sample;
+
+public class Hello {
+	
+	
+	    public static void main(String[] args) {
+	        
+	        System.out.println("Hello, this is tns training session");
+	    }
+	}
+
+
+
